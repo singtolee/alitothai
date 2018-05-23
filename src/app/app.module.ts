8e379b416e29d3fac1f612bcf17d8454a0e6af71
singtolee <liqiang0618@outlook.com>
2018-05-23 15:42:27 +0700

    enabled Firebase Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { KeyWordComponent } from './key-word/key-word.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { EmbedWebviewComponent } from './embed-webview/embed-webview.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment.prod';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAt4JqhPWFqgn4Voc971zIR1nzQ9XMpKAo",
+  authDomain: "restock-helper.firebaseapp.com",
+  databaseURL: "https://restock-helper.firebaseio.com",
+  projectId: "restock-helper",
+  storageBucket: "restock-helper.appspot.com",
+  messagingSenderId: "966464179859"
+};
 
 @NgModule({
   declarations: [
@@ -16,9 +33,14 @@ import { EmbedWebviewComponent } from './embed-webview/embed-webview.component';
     EmbedWebviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/key-word/key-word.component.ts
+++ src/app/key-word/key-word.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
+export interface Item {name: string;}
 
 @Component({
   selector: 'app-key-word',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeyWordComponent implements OnInit {
 
-  constructor() { }
+  keywords:Observable<Item[]>;
+  private keywordsCollection: AngularFirestoreCollection<Item>;
+  private keyword:string='';
+
+  constructor(private db:AngularFirestore) {
+    this.keywordsCollection = db.collection<Item>('KEY-WORDS');
+    this.keywords = this.keywordsCollection.valueChanges();
+  }
 
   ngOnInit() {
+  }
+
+  saveKeyWord(){
+    this.keyword = this.keyword.trim();
+    if(!this.keyword){
+      return;
+    }
+    this.db.collection('KEY-WORDS').add({'name':this.keyword});
+    this.keyword = '';
   }
 
 }
